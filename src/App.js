702529c5db44c0ef3b77bf203de276e66b95c8d8
katsu yonezawa <katsuyonezawa@katsunoMacBook-Air.local>
2023-08-30 23:14:51 +0900

    0

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
+import React from "react";
+
 function MyButton() {
-  return (
-    <button>
-      I'm a button
-    </button>
-  );
+  return <button>I'm a button</button>;
 }
 
 export default function MyApp() {
